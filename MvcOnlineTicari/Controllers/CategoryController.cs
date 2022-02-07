c6746622d9a554d809ac37f59a42a1df3fb13860
BatuhanB <balibatuhan19@gmail.com>
2022-02-07 17:46:52 +0300

    adding admin page and new controller

---
+++ MvcOnlineTicari/Controllers/CategoryController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcOnlineTicari.Models.Entity;
+using MvcOnlineTicari.Models.Context;
+
+namespace MvcOnlineTicari.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        Context context = new Context();
+        public ActionResult Index()
+        {
+            var values =context.Categories.ToList();    
+            return View(values);
+        }
+        [HttpGet]
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddCategory(Category category)
+        {
+            context.Categories.Add(category);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult DeleteCategory(int id)
+        {
+            var category = context.Categories.Find(id);
+            context.Categories.Remove(category);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
