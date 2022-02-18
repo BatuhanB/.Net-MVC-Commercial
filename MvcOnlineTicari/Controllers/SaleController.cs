0f9198eecb7e01713b28c25c71c6b020fce7e261
BatuhanB <balibatuhan19@gmail.com>
2022-02-18 20:45:06 +0300

    adding sale update and sale detail pages

--- MvcOnlineTicari/Controllers/SaleController.cs
+++ MvcOnlineTicari/Controllers/SaleController.cs
@@ -54,5 +54,53 @@ namespace MvcOnlineTicari.Controllers
             context.SaveChanges();
             return RedirectToAction("Index");
         }
+        public ActionResult GetSale(int id)
+        {
+            List<SelectListItem> value1 = (from x in context.Products.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.ProductName,
+                                               Value = x.ProductID.ToString()
+                                           }).ToList();
+
+            List<SelectListItem> value2 = (from x in context.Currents.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.CurrentName,
+                                               Value = x.CurrentID.ToString()
+                                           }).ToList();
+
+            List<SelectListItem> value3 = (from x in context.Employees.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.EmployeeName + " " + x.EmployeeSurName,
+                                               Value = x.EmployeeID.ToString()
+                                           }).ToList();
+
+            ViewBag.prdcts = value1;
+            ViewBag.crrnts = value2;
+            ViewBag.emplys = value3;
+            var values = context.SaleBehaviors.Find(id);
+            return View("GetSale", values);
+        }
+        public ActionResult UpdateSale(SaleBehavior sale)
+        {
+            if (!ModelState.IsValid) { return View("GetSale"); }
+
+            var sales = context.SaleBehaviors.Find(sale.SaleID);
+            sales.ProductID = sale.ProductID;
+            sales.CurrentID = sale.CurrentID;
+            sales.EmployeeID = sale.EmployeeID;
+            sales.SaleQuantity = sale.SaleQuantity;
+            sales.SaleSumAmount = sale.SaleSumAmount;
+            sales.SaleDate = sale.SaleDate;
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult SaleDetail(int id)
+        {
+            var values = context.SaleBehaviors.Where(x => x.SaleID == id).ToList();
+            return View(values);
+        }
     }
 }
