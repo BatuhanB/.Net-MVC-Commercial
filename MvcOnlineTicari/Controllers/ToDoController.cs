e5adefe6f128e24a21367d219390733156cbc76e
BatuhanB <balibatuhan19@gmail.com>
2022-03-01 21:45:01 +0300

    editing toDo page and receiving data

--- MvcOnlineTicari/Controllers/ToDoController.cs
+++ MvcOnlineTicari/Controllers/ToDoController.cs
@@ -3,15 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MvcOnlineTicari.Models.Entity;
+using MvcOnlineTicari.Models.Context;
 
 namespace MvcOnlineTicari.Controllers
 {
     public class ToDoController : Controller
     {
         // GET: ToDo
+        Context context = new Context();
         public ActionResult Index()
         {
-            return View();
+            var value1 = context.Currents.Count().ToString();
+            ViewBag.crrnts = value1;
+            var value2 = context.Products.Count().ToString();
+            ViewBag.prdcts = value2;
+            var value3 = context.Categories.Count().ToString();
+            ViewBag.ctgrs = value3;
+            var value4 = (from x in context.Currents select x.CurrentCity).Distinct().Count().ToString();
+            ViewBag.cities = value4;
+            var todos = context.ToDoLists.ToList();
+            return View(todos);
         }
     }
 }
