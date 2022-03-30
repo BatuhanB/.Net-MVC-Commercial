dba7f48e3adc6fe7a8d16e670eb85622e38c35de
BatuhanB <balibatuhan19@gmail.com>
2022-03-30 20:02:02 +0300

    dynamic invoices has been added

--- MvcOnlineTicari/Controllers/InvoiceController.cs
+++ MvcOnlineTicari/Controllers/InvoiceController.cs
@@ -73,9 +73,41 @@ namespace MvcOnlineTicari.Controllers
         {
             context.InvoiceItems.Add(invoiceItem);
             context.SaveChanges();
-            var invoiceID = context.Invoices.Where(x=>x.InvoiceID == invoiceItem.InvoiceItemID).Select(x => x.InvoiceID).FirstOrDefault();
+            var invoiceID = context.Invoices.Where(x => x.InvoiceID == invoiceItem.InvoiceItemID).Select(x => x.InvoiceID).FirstOrDefault();
             ViewBag.invcID = invoiceID;
             return RedirectToAction("Index");
+        }
+        public ActionResult Dynamic()
+        {
+            Class4 class4 = new Class4();
+            class4.val1 = context.Invoices.ToList();
+            class4.val2 = context.InvoiceItems.ToList();
+            return View(class4);
+        }
+        public ActionResult SaveInvoice(string InvoiceSerialNo, string InvoiceQueueNo, DateTime InvoiceDate,string InvoiceTaxAuthority,string InvoiceTime,string InvoiceBillTo,string InvoiceShipTo,string InvoiceSumAmount, InvoiceItem[] invoiceItems)
+        {
+            Invoice invoice = new Invoice();
+            invoice.InvoiceSerialNo = InvoiceSerialNo;
+            invoice.InvoiceQueueNo = InvoiceQueueNo;
+            invoice.InvoiceDate = InvoiceDate;
+            invoice.InvoiceTaxAuthority = InvoiceTaxAuthority;
+            invoice.InvoiceTime = InvoiceTime;
+            invoice.InvoiceBillTo = InvoiceBillTo;
+            invoice.InvoiceShipTo = InvoiceShipTo;
+            invoice.InvoiceSumAmount = decimal.Parse(InvoiceSumAmount);
+            context.Invoices.Add(invoice);
+            foreach(var x in invoiceItems)
+            {
+                InvoiceItem invoiceItem = new InvoiceItem();
+                invoiceItem.InvoiceItemDesc = x.InvoiceItemDesc;
+                invoiceItem.InvoiceItemUnitPrice = x.InvoiceItemUnitPrice;
+                invoiceItem.InvoiceItemQuantity = x.InvoiceItemQuantity;
+                invoiceItem.InvoiceItemID = x.InvoiceItemID;
+                invoiceItem.InvoiceItemPrice = x.InvoiceItemPrice;
+                context.InvoiceItems.Add(invoiceItem);
+            }
+            context.SaveChanges();
+            return Json("İşlem Başarılı",JsonRequestBehavior.AllowGet);
         }
     }
 }
---
+++ MvcOnlineTicari/Models/Entity/Class4.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcOnlineTicari.Models.Entity
+{
+    public class Class4
+    {
+        public IEnumerable<Invoice> val1 { get; set; }
+        public IEnumerable<InvoiceItem> val2 { get; set; }
+    }
+}
